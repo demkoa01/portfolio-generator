faaeb273f759fd7ed6e68ec37592c96a026b8076
Allison Demkovich <demkovich.allison@gmail.com>
2022-01-24 15:58:20 -0600

    generate HTML file

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i=0; i<profileDataArr.length; i++){
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('==========');
-
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw new Error(err);
+    console.log('Portfolio complete! Checkout index.html to see the output!');
+});
