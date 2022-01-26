b7c3b89bacf781a0eae63ee8cf2bb4c47cb7af70
Allison Demkovich <demkovich.allison@gmail.com>
2022-01-25 18:49:01 -0600

    update HTML to include projects

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const { prompt } = require('inquirer');
 const inquirer = require('inquirer');
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile('./index.html', pageHTML, err => {
@@ -142,6 +142,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = generatePage(portfolioData);
+
+        fs.writeFile('./index.html', pageHTML, err => {
+            if (err) throw new Error(err);
+            console.log('Portfolio complete! Checkout index.html to see the output!');
+        });
     });
 
